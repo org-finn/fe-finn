5ad5059f4e26f598c729a6d05a693bfaa7e94d94
Hyoeunkh <lhe4528@knu.ac.kr>
2025-06-08 22:27:33 +0900

    design: 폰트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,23 @@ import DetailPage from './pages/Detail';
 import NewsBoardPage from './pages/NewsBoard';
 import FinnGPTPage from './pages/FinnGPT';
 import MyPage from './pages/My';
+import GlobalStyle from './global';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route index path="/" element={<MainPage />} />
-        <Route path="/stock" element={<StockPage />} />
-        <Route path="/stock/:id" element={<DetailPage />} />
-        <Route path="/news" element={<NewsBoardPage />} />
-        <Route path="/finngpt" element={<FinnGPTPage />} />
-        <Route path="/my" element={<MyPage />} />
-      </Route>
-    </Routes>
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index path="/" element={<MainPage />} />
+          <Route path="/stock" element={<StockPage />} />
+          <Route path="/stock/:id" element={<DetailPage />} />
+          <Route path="/news" element={<NewsBoardPage />} />
+          <Route path="/finngpt" element={<FinnGPTPage />} />
+          <Route path="/my" element={<MyPage />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
