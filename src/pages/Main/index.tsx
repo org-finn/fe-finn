f58a9b8709d27d052d31fd95fa344cb910ea8d13
userjmmm <summer7j@naver.com>
2025-09-03 17:26:25 +0900

    refactor: 메인페이지에 risingData section 제거

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -4,16 +4,12 @@ import { Paragraph } from '@/components/common/typography/Paragraph';
 import TickerList from '@/components/Ticker/TickerList';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
-import {
-  useGetPopularTickers,
-  useGetRisingTickers,
-} from '@/api/hooks/useGetTickerList';
+import { useGetPopularTickers } from '@/api/hooks/useGetTickerList';
 import MarketStatusBanner from '@/components/common/Banner/MarketStatusBanner';
 
 export default function MainPage() {
   const navigate = useNavigate();
   const { data: popularData } = useGetPopularTickers();
-  const { data: risingData } = useGetRisingTickers();
   return (
     <Wrapper>
       <SearchBar />
@@ -37,26 +33,6 @@ export default function MainPage() {
           </MoreBtn>
         </TickerTitle>
         <TickerList items={popularData?.content.predictionList || []} />
-      </TickerSection>
-      <TickerSection>
-        <TickerTitle>
-          <Left>
-            <Paragraph size="m" weight="bold">
-              가장 많이 오르는 Top5
-            </Paragraph>
-            <Paragraph size="xxs" weight="normal" variant="grey">
-              Finn이 내일 가장 많이 오르는 종목을 예측했어요!
-            </Paragraph>
-          </Left>
-          <MoreBtn
-            aria-label="more-ticker-btn"
-            onClick={() => navigate('/ticker')}
-          >
-            전체 보기
-            <IoIosArrowForward />
-          </MoreBtn>
-        </TickerTitle>
-        <TickerList items={risingData?.content.predictionList || []} />
       </TickerSection>
     </Wrapper>
   );
