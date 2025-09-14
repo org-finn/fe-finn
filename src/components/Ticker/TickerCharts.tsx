99ee1681c9d36967f0e1e26c2697cfe10a87f163
userjmmm <summer7j@naver.com>
2025-09-14 12:48:09 +0900

    refactor: 실제 주가 그래프 모바일의 경우 padding 없애기

--- src/components/Ticker/TickerCharts.tsx
+++ src/components/Ticker/TickerCharts.tsx
@@ -254,4 +254,8 @@ const Wrapper = styled.div<{ $sentiment: number }>`
   .apexcharts-area-series .apexcharts-series-0 .apexcharts-area {
     fill: url(#SvgjsLinearGradient1000) !important;
   }
+
+  @media screen and (max-width: 768px) {
+    padding: 0px;
+  }
 `;
