3645fd9d27b6072187f69d389431dc740a9f7edb
userjmmm <summer7j@naver.com>
2026-01-28 16:31:50 +0900

    design: 구글 로그인 버튼 가이드라인에 맞는 로고 및 폰트 추가

--- src/global.ts
+++ src/global.ts
@@ -17,6 +17,14 @@ const GlobalStyle = createGlobalStyle`
 		font-display: swap;
 	}
 
+	@font-face {
+		font-family: "Roboto";
+		font-weight: 500;
+		src: url("./assets/fonts/Roboto-Medium.woff2") format("woff2"),
+		url("./assets/fonts/Roboto-Medium.woff") format("woff"),;
+		font-display: swap;
+	}
+
     html, body, div, span, applet, object, iframe,
     h1, h2, h3, h4, h5, h6, p, blockquote, pre,
     a, abbr, acronym, address, big, cite, code,
