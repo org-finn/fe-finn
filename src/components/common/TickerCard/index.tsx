f9cf6bce75ed6cd40a610e7e525ef1403b313a71
userjmmm <summer7j@naver.com>
2026-03-02 01:26:52 +0900

    feat: 첫 회원가입 시 관심종목 선택하는 joinPage 구현

--- src/components/common/TickerCard/index.tsx
+++ src/components/common/TickerCard/index.tsx
@@ -172,7 +172,7 @@ const LikeIcon = styled.div`
   height: 30px;
   right: 2px;
   top: 4px;
-  z-index: 100;
+  z-index: 9;
   cursor: pointer;
 
   @media screen and (max-width: 768px) {
--- src/pages/Join/index.tsx
+++ src/pages/Join/index.tsx
@@ -1,23 +1,149 @@
-import { useEffect } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import useAuth from '@/hooks/useAuth';
+import styled from 'styled-components';
+import { Paragraph } from '@/components/common/typography/Paragraph';
+import { Text } from '@/components/common/typography/Text';
+import TickerCard from '@/components/common/TickerCard';
+import Pagination from '@/components/common/Pagination';
+import NoItem from '@/components/common/Layout/NoItem';
+import Button from '@/components/common/Button';
+import { useGetJoinTickerList } from '@/api/hooks/useGetJoinTickerList';
+import { usePutFavoriteTickers } from '@/api/hooks/usePutFavoriteTickers';
+import useIsMobile from '@/hooks/useIsMobile';
 
 export default function JoinPage() {
   const navigate = useNavigate();
-  const { isAuthenticated } = useAuth();
+  const isMobile = useIsMobile();
+  const [selectedTickers, setSelectedTickers] = useState<Set<string>>(
+    new Set()
+  );
+  const { mutateAsync: postMultipleLikes } = usePutFavoriteTickers();
+  const [currentPage, setCurrentPage] = useState(1);
+  const TOTAL_PAGES = 4;
+  const { data } = useGetJoinTickerList(currentPage - 1);
+  const tickerList = data?.content.tickers || [];
 
-  // to-do: 추후 초기 설정 컴포넌트 추가해야 됨
-  useEffect(() => {
-    if (isAuthenticated) {
-      const redirectPath = localStorage.getItem('redirectPath');
-      if (redirectPath) {
-        localStorage.removeItem('redirectPath');
-        navigate(redirectPath, { replace: true });
-      } else {
-        navigate('/', { replace: true });
+  const handlePageChange = (pageNum: number) => {
+    setCurrentPage(pageNum);
+  };
+
+  const handleSkip = () => {
+    navigate('/');
+  };
+
+  const handleStart = async () => {
+    try {
+      if (selectedTickers.size > 0) {
+        await postMultipleLikes({
+          tickers: [...selectedTickers],
+        });
       }
+    } catch (error) {
+      console.error('좋아요 처리 중 오류 발생:', error);
+    } finally {
+      navigate('/');
     }
-  }, [isAuthenticated, navigate]);
+  };
 
-  return null;
+  const handleToggleLike = (tickerCode: string, isFavorite: boolean) => {
+    setSelectedTickers((prev) => {
+      const newSet = new Set(prev);
+      if (isFavorite) {
+        newSet.add(tickerCode);
+      } else {
+        newSet.delete(tickerCode);
+      }
+      return newSet;
+    });
+  };
+
+  const buttonStyle = {
+    width: isMobile ? '140px' : '170px',
+    height: isMobile ? '40px' : '46px',
+    fontSize: isMobile ? '16px' : '18px',
+  };
+
+  return (
+    <Wrapper>
+      <Paragraph size="m" weight="bold">
+        관심 있는
+        <Text size="l" weight="bold" variant="blue">
+          종목
+        </Text>
+        을 선택하세요!
+      </Paragraph>
+      {tickerList.length === 0 ? (
+        <NoItem
+          message="종목 정보가 없어요!"
+          height={300}
+          alignItems="center"
+        />
+      ) : (
+        <CardContainer>
+          {tickerList.map((ticker) => (
+            <TickerCard
+              key={ticker.tickerCode}
+              tickerCode={ticker.tickerCode}
+              shortCompanyName={ticker.shortCompanyName}
+              predictionStrategy={ticker.predictionStrategy}
+              sentiment={ticker.sentiment}
+              graphData={ticker.graphData}
+              onToggleLike={handleToggleLike}
+              isSelected={selectedTickers.has(ticker.tickerCode)}
+            />
+          ))}
+        </CardContainer>
+      )}
+      <Pagination
+        currentPage={currentPage}
+        totalPages={TOTAL_PAGES}
+        onPageChange={handlePageChange}
+      />
+      <ButtonWrapper>
+        <Button
+          aria-label="건너뛰기"
+          variant="blackOutline"
+          style={buttonStyle}
+          onClick={handleSkip}
+        >
+          건너뛰기
+        </Button>
+        <Button
+          aria-label="시작하기"
+          variant="mint"
+          style={buttonStyle}
+          onClick={handleStart}
+        >
+          시작하기
+        </Button>
+      </ButtonWrapper>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  padding: 20px;
+`;
+
+const CardContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(170px, 1fr));
+  gap: 20px;
+  margin-top: 20px;
+`;
+
+const ButtonWrapper = styled.div`
+  width: 630px;
+  display: flex;
+  justify-content: space-between;
+  gap: 180px;
+  margin-bottom: 30px;
+
+  @media screen and (max-width: 768px) {
+    margin-bottom: 20px;
+  }
+`;
