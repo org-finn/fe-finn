0eaec366e0ae71a65559f88a5ad0bd18f9dd723f
userjmmm <summer7j@naver.com>
2026-03-02 13:26:51 +0900

    feat: 메인헤더에 마이페이지로 이동하는 유저 프로필 버튼 추가

--- src/components/common/Layout/MainLayout.tsx
+++ src/components/common/Layout/MainLayout.tsx
@@ -3,14 +3,14 @@ import { Suspense, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { QueryErrorResetBoundary } from '@tanstack/react-query';
 import { ErrorBoundary } from 'react-error-boundary';
-import MainHeader from './MainHeader';
+import MainHeader from './Header.tsx/MainHeader';
 import Footer from './Footer';
 import useScrollToTop from '@/hooks/useScrollToTop';
 import ErrorComponent from './Error';
 import Loading from './Loading';
 import MainSkeleton from '@/components/Main/MainSkeleton';
 import TickerSkeleton from '@/components/Ticker/TickerSkeleton';
-import SubHeader from './SubHeader';
+import SubHeader from './Header.tsx/SubHeader';
 import useIsTouchDevice from '@/hooks/useIsTouchDevice';
 import useScrollDirection from '@/hooks/useScrollDirection';
 
